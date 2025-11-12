cc426a0b751f5514a0b9669b3c605bd5a0e84cda
Dylan Ferrell <ferrelld206@gmail.com>
2025-11-12 19:01:29 +0700

    first commit

---
+++ temperature.cpp
@@ -0,0 +1,93 @@
+#include <iostream> 
+#include <iomanip> 
+#include <string> 
+
+double celciustofarenheit(double x)
+{
+    return x * 9/5 + 32;
+}
+
+double farenheittocelcius(double x)
+{
+    return (x - 32) * 5/9;
+}
+
+double kelvintocelcius(double x)
+{
+    return x - 273.15;
+}
+
+double celciustokelvin(double x)
+{
+    return x + 273.15;
+}
+
+double kelvintofarenheit(double x)
+{
+    return (x - 273.15) * 9/5 + 32;
+}
+
+double farenheittokelvin(double x)
+{
+    return ((x - 32) * 5/9 + 273.15);
+}
+
+int main() 
+{
+    bool start { 1 };
+    std::cout << std::fixed << std::setprecision(2);
+
+    do 
+    {
+        std::cout << "Enter temperature: " << '\n';
+        double utemp{ };
+        std::cin >> utemp; 
+        
+        std::cout << "Input Unit: " << '\n';
+        std::string input{ };
+        std::cin >> input; 
+        
+        std::cout << "Output Unit: " << '\n';
+        std::string output{ };
+        std::cin >> output; 
+        
+        if ((output == "F" || output == "f") && (input == "C" || input == "c"))
+                std::cout << utemp << input << " is " << celciustofarenheit(utemp) << output << '\n';
+
+        else if ((output == "C" || output == "c") && (input == "F" || input == "f" ))
+                std::cout << utemp << input << " is " << farenheittocelcius(utemp) << output << '\n';
+
+        else if ((output == "K" || output == "k") && (input == "C" || input == "c" ))
+                std::cout << utemp << input << " is " << celciustokelvin(utemp) << output << '\n';
+
+        else if ((output == "C" || output == "c") && (input == "K" || input == "k" ))
+                std::cout << utemp << input << " is " << kelvintocelcius(utemp) << output << '\n';
+
+        else if ((output == "K" || output == "k") && (input == "F" || input == "f" ))
+                std::cout << utemp << input << " is " << farenheittokelvin(utemp) << output << '\n';
+
+        else if ((output == "F" || output == "f") && (input == "K" || input == "k" ))
+                std::cout << utemp << input << " is " << kelvintofarenheit(utemp) << output << '\n';
+        else 
+            std::cout << "Enter a valid temperature character! C, K, or F!" << '\n';
+            continue;
+
+    std::string again { }; 
+    std::cout << "Convert again? (y/n): " << '\n';
+    std::cin >> again; 
+    
+
+        {if (again == "y")
+            start = 1;
+    
+        else 
+            {
+            start = 0;
+                std::cout << "Goodbye!" << '\n';
+            }
+        }
+    } while (start == 1);
+
+return 0;
+
+}
